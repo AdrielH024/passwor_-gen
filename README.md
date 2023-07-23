6b42abb54727f5a8dabeeaa039b8b196f2d3d129
AdrielH024 <adriel.henrique024@gmail.com>
2023-07-23 06:40:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+it's just a password generetor in python, he write a piece of password using  aleatory characteres 
---
+++ passwor_ gen.py
@@ -0,0 +1,30 @@
+import random
+
+normalchar= "abcdefghijklmnopqrstuvwxyz"
+upperchar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+espec= "@!#$%+"
+
+i = int(input("number of characteres in new password?\n"))
+j=0
+
+newpass = []
+
+while(j <= i):
+ typechar = random.randint(1,4)
+ if(typechar == 1):
+     charn = random.randint(0,22)
+     newpass.append(normalchar[charn])
+ if(typechar == 2):
+     charn = random.randint(0,22)
+     newpass.append(upperchar[charn])
+ if(typechar == 3):
+     charn = random.randint(0,9)
+     newpass.append(numbers[charn])
+ if(typechar == 4):
+     charn = random.randint(0,5)
+     newpass.append(espec[charn])
+ j = j+1
+ 
+newpass = ''.join(newpass)
+print (newpass)
